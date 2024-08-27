29332a049c4b92d3cbd8ff10710b90a72e1e66f4
SnowDango <xdi.grand.you.winner@gmail.com>
2024-08-28 08:36:30 +0900

    とりまテスト

--- presenter/playing/src/main/java/com/snowdango/sumire/presenter/playing/PlayingScreen.kt
+++ presenter/playing/src/main/java/com/snowdango/sumire/presenter/playing/PlayingScreen.kt
@@ -251,7 +251,7 @@ fun PreviewPlayingSongComponent() {
     SumireTheme {
         PlayingSongComponent(
             artwork = null,
-            title = "title",
+            title = "title2",
             album = "album",
             artist = "artist",
             app = MusicApp.APPLE_MUSIC
