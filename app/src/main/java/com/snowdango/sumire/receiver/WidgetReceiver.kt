37f172f64553abf46258f8daef221582166f9dcc
SnowDango <xdi.grand.you.winner@gmail.com>
2024-09-22 01:28:40 +0900

    create Receiver

---
+++ app/src/main/java/com/snowdango/sumire/receiver/WidgetReceiver.kt
@@ -0,0 +1,9 @@
+package com.snowdango.sumire.receiver
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class WidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget
+        get() = TODO("Not yet implemented")
+}
