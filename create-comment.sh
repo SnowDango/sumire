f49d69bfd52e4938df99def0c6a6805e9fabb8b9
SnowDango <xdi.grand.you.winner@gmail.com>
2024-12-25 18:21:42 +0900

    markdown test

--- create-comment.sh
+++ create-comment.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-touch comment.txt
-length=$(cat artifact.json | jq -c '.items | length')
+comment_file=comment.md
+touch $comment_file
+length=$(cat compare.json | jq -c '.[] | length')
 echo $length
 if [ $length -gt 0 ]; then
-  echo "VRT Result" > comment.txt
-  echo "| changed |" >> comment.txt
-  echo "|-------|" >> comment.txt
-  cat artifact.json | \
-  jq -c '.items[]' | while read artifact; do
-    echo "| [$(echo $artifact | jq -r '.path')]($(echo $artifact | jq -r '.url')) |" >> comment.txt
+  echo "VRT Result" > $comment_file
+  echo "| changed |" >> $comment_file
+  echo "|-------|" >> $comment_file
+  cat compare.json | \
+  jq -c '.[]' | while read artifact; do
+    echo "| ![compare.png](data:image/jpeg;base64,$artifact) |" >> $comment_file
+    # echo "| [$(echo $artifact | jq -r '.path')]($(echo $artifact | jq -r '.url')) |" >> $comment_file
   done
 else
-  echo "VRT Result" > comment.txt
-  echo "not changed screen" >> comment.txt
+  echo "VRT Result" > $comment_file
+  echo "not changed screen" >> $comment_file
 fi
