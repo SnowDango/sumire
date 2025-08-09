83a09b9e7118d80e8475fae10cf7af27ca2d4e23
SnowDango <xdi.grand.you.winner@gmail.com>
2025-08-10 06:53:17 +0900

    jvm関係のupdate

--- repository/build.gradle.kts
+++ repository/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        jvmToolchain(17)
     }
 }
 
@@ -44,7 +44,7 @@ dependencies {
     implementation(libs.bundles.ktor.client)
     implementation(libs.bundles.paging)
     implementation(libs.bundles.slf4j)
-    
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
